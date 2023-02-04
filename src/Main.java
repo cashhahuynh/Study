25c33a0e7713a88afe1917099f40c7f404a3fb10
cashhahuynh <cashhahuynh@gmail.com>
2023-02-04 09:57:52 -0500

    int to roman numeral coding challenge commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import CodingChallenges.IntegerToRomanNumeral;
+
 public class Main {
 
     public static void main(String[] arg) {
-
+        System.out.println(IntegerToRomanNumeral.intToRomanNumeral(345));
     }
 
 }
