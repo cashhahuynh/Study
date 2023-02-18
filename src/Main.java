1b9fb2b2dd0adcc38b4fdaf0579fd90538cc2589
cashhahuynh <cashhahuynh@gmail.com>
2023-02-18 10:24:47 -0500

    pangram check coding challenge commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import CodingChallenges.Pangram;
-
 public class Main {
 
     public static void main(String[] arg) {
-        System.out.println(Pangram.isPangram("Aa78bcdefghiiiijk  lmnopqrstuvwxyz!?;"));
+
     }
 
 }
