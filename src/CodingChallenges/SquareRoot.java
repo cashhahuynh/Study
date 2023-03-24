3a79294e658ab5c8cce56f576748ebf35e5c00f9
cashhahuynh <cashhahuynh@gmail.com>
2023-03-23 23:12:57 -0400

    square root coding challenge and input test commit

--- src/CodingChallenges/SquareRoot.java
+++ src/CodingChallenges/SquareRoot.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 
 public class SquareRoot {
 
-    public static void squareRoot() {
+    public static String squareRoot() {
 
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Please provide a number to find its square root: ");
-
         Double num = scanner.nextDouble();
+        scanner.close();
+
+        String answer = returnSquareRoot(num);
+        return answer;
+
+    }
+
+    public static String returnSquareRoot(double num) {
 
         Double answer = Math.sqrt(num);
+        String returnAnswer = String.format("The square root of %f is %f", num, answer);
 
-        System.out.printf("The square root of %f is %f", num, answer);
+        return returnAnswer;
 
     }
 
--- src/CodingChallenges/Tests/SquareRootTest.java
+++ src/CodingChallenges/Tests/SquareRootTest.java
@@ -1,12 +1,29 @@
 package CodingChallenges.Tests;
 
+import CodingChallenges.SquareRoot;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
 
 public class SquareRootTest {
 
     @Test
     public void squareRootTest() {
-        
+
+        String input = "16";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+
+        String expect = SquareRoot.returnSquareRoot(Integer.parseInt(input));
+        String answer = SquareRoot.squareRoot();
+
+        assertEquals(expect, answer);
+
     }
 
 }
