e05478d8a1cb402ec6cd8cdd2ab94b4149f4f98b
cashhahuynh <cashhahuynh@gmail.com>
2023-03-28 21:23:46 -0400

    regex 3/28 commit

--- src/RegularExpression/PatternAndMatcher.java
+++ src/RegularExpression/PatternAndMatcher.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class PatternAndMatcher {
 
-    public static void patternAndMatcher() {
+    public static void patternAndMatcherBasics() {
 
         String regex = "[aeiou]";
 
@@ -36,7 +36,46 @@ public class PatternAndMatcher {
         //https://jenkov.com/tutorials/java-regex/matcher.html
         //TO DO: learn about group, multiple groups, groups inside groups
         //TO DO: learn about replaceAll() + replaceFirst()
-        //TO DO: appendReplacement() + appendTail()
+
+    }
+
+    public static void regexGroups() {
+
+        String text = "John writes about this, and john writes about that, and John writes about everything.";
+
+        String group = "(John)";
+
+        Pattern pattern = Pattern.compile(group, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        int counter = 0;
+        while(matcher.find()) {
+            counter ++;
+            System.out.printf("\nsingle group found #%d: " + matcher.group(1), counter);
+        }
+
+    }
+
+    public static void appendMethods() {
+
+        String text = "John Johnson writes about this, and John Doe writes about that, and John Wayne writes about everything.";
+
+        String group = "((John) (.+?)) ";
+
+        Pattern pattern = Pattern.compile(group);
+        Matcher matcher = pattern.matcher(text);
+        StringBuffer sb = new StringBuffer();
+
+        int counter = 0;
+        while(matcher.find()) {
+            counter ++;
+            matcher.appendReplacement(sb, "Joe Blocks ");
+            System.out.printf("\nexample of appendReplacement #%d: " + sb.toString(), counter);
+        }
+
+        matcher.appendTail(sb);
+
+        System.out.println("\nexample of appendTail: " + sb.toString());
 
     }
 
